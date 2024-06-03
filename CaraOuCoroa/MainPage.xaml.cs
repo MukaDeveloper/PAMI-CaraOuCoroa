8df862935179d56269815ffd2fff3d0cc3cfcb7e
MukaDeveloper <muka.bertoni@gmail.com>
2024-06-03 19:43:59 -0300

    projeto finalizado

---
+++ CaraOuCoroa/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CaraOuCoroa.Models;
+
+namespace CaraOuCoroa
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SidePicker.SelectedIndex = 0;
+        }
+
+        private void CoinFlipButton_Clicked(object sender, EventArgs e)
+        {
+            string escolha = SidePicker.SelectedItem.ToString();
+            string sorteado = new Coin().Girar();
+            CoinImage.Source = sorteado.ToLower()+".jpg";
+            ResultLabel.Text = escolha == sorteado ? $"Parabéns! Deu {sorteado}, você ganhou!" : $"Que pena, deu {sorteado}, você perdeu!";
+        }
+    }
+
+}
